bdcbd436b2534ab97c84c1f51a890b415859fc3a
Caitano <josiasmartinscaitano@gmail.com>
2023-02-08 18:14:31 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -188,4 +188,24 @@ describe('', () => {
     expect(httpResponse.body).toEqual(new ServerError());
   })
 
+  test('should return 200 if  valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'valid_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@gmail.com',
+      password: 'valid_password'
+    })
+  });
+
 })
