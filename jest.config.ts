e0d607bfa4c9120635979bf71afe7332dc3d5e18
Caitano <josiasmartinscaitano@gmail.com>
2023-02-08 19:01:48 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -12,3 +12,12 @@ export default {
     '.+\\.ts$': 'ts-jest'
   }
 }
+
+// import type {Config} from 'jest';
+// import {defaults} from 'jest-config';
+
+// const config: Config = {
+//   moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+// };
+
+// export default config;
