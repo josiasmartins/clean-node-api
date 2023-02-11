dfa3cb486d3c4ac8564fce561bf82fd365a9e21c
Caitano <josiasmartinscaitano@gmail.com>
2023-02-11 20:43:47 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,9 +1,23 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 // para ter acesso fora do dominio
 
 describe('SignUp Routes', () => {
+
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__)
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('should return an account on success', async () => {
     await request(app)
       .post('/test_cors')
