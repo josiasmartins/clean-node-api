26f7195d8737acf668f7c1f13fb1cc48d0b9bd91
Caitano <josiasmartinscaitano@gmail.com>
2023-02-11 21:16:14 -0300

    feat: add SignUp Facory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from '../../presentation/controller/signup/signup' 
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account';
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter';
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSigUpController = (): SignUpController => {
+  const salt = 12;
+  const emailValidatorAdapter = new EmailValidatorAdapter();
+  const bCrypterAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bCrypterAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount);
+  return signUpController
+}
